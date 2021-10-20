1d8e65958a1d6933ceda7d942d279f9902f515e7
Heili Lowman <heili.lowman@gmail.com>
2021-10-20 14:29:22 -0700

    Load in data.

--- acf_ccf_script.R
+++ acf_ccf_script.R
@@ -2,3 +2,13 @@
 ## October 20
 ## Betsy Summers, Heili Lowman
 
+# Load packages
+library(tidyverse)
+library(here)
+
+# Load data
+dat <- read_csv("selected_autotrophic_rivers_daily.csv")
+
+# Select Canyon Creek data
+cc <- dat %>%
+  filter(sitecode == "nwis_10133650")
