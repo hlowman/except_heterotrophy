a147695eb1d4df40bb4a10155e9d790e3d07ae15
Heili Lowman <heili.lowman@gmail.com>
2021-10-20 14:39:03 -0700

    Added figure to check for data availability.

--- acf_ccf_script.R
+++ acf_ccf_script.R
@@ -1,5 +1,5 @@
 ## Correlation Analysis Script
-## October 20
+## October 20, 2021
 ## Betsy Summers, Heili Lowman
 
 # Load packages
@@ -12,3 +12,15 @@ dat <- read_csv("selected_autotrophic_rivers_daily.csv")
 # Select Canyon Creek data
 cc <- dat %>%
   filter(sitecode == "nwis_10133650")
+
+# Make a quick plot of GPP and ER
+figure1 <- ggplot(cc, aes(x = Date)) +
+  geom_line(aes(y = GPP), color = "green") +
+  geom_line(aes(y = ER), color = "black") +
+  labs(y = "ER / GPP")
+
+# We will be basing our analysis off of Diamond et al. 2021.
+# But here are some additional resources:
+# https://nwfsc-timeseries.github.io/atsa-labs/sec-tslab-correlation-within-and-among-time-series.html
+# http://r-statistics.co/Time-Series-Analysis-With-R.html
+
