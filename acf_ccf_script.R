dd573f0c35121fcfdcf8c4e68b98175704ccb586
Heili Lowman <heili.lowman@gmail.com>
2021-11-01 16:19:09 -0700

    Tidied up ccf script.

--- acf_ccf_script.R
+++ acf_ccf_script.R
@@ -5,6 +5,7 @@
 # Load packages
 library(tidyverse)
 library(here)
+library(tseries)
 
 # Load data
 dat <- read_csv("selected_autotrophic_rivers_daily.csv")
@@ -17,21 +18,34 @@ cc12 <- dat %>%
   filter(sitecode == "nwis_10133650" & year == 2012)
 
 # Make a quick plot of GPP and ER
-figure1 <- ggplot(cc11, aes(x = Date)) +
+(figure1 <- ggplot(cc11, aes(x = Date)) +
   geom_line(aes(y = GPP), color = "green") +
   geom_line(aes(y = ER), color = "black") +
-  labs(y = "ER / GPP")
+  labs(y = "ER / GPP"))
 
 # We will be basing our analysis off of Diamond et al. 2021.
 # But here are some additional resources:
 # https://nwfsc-timeseries.github.io/atsa-labs/sec-tslab-correlation-within-and-among-time-series.html
 # http://r-statistics.co/Time-Series-Analysis-With-R.html
 
-# First, we'll calculate the lagged differences, as Diamond did.
+# Tests for stationarity
+# Augmented Dickey-Fuller test
+adf.test(cc11$GPP) # apparently doesn't play nice with missing data
+# so, to run some of the same analyses, we will need to impute as
+# Diamond did
+
+# Kwiatkowski-Phillips-Schmidt-Shin test
+kpss.test(cc11$GPP) # skips missing data
+# p < 0.01 which would suggest the dataset is non-stationary
+# kpss tests check for stationarity, but can do so in the presence
+# of a deterministic trend: https://www.machinelearningplus.com/time-series/kpss-test-for-stationarity/
+
+# We'll calculate the lagged differences, as Diamond did.
+# Only doing it for one year for now (2011).
 diff_c11_er <- diff(-cc11$ER,1)
 diff_c11_gpp <- diff(cc11$GPP,1)
 
-# And creat vectors for the raw data.
+# And create vectors for the raw data.
 cc11_er <- -cc11$ER
 cc11_gpp <- cc11$GPP
 
@@ -56,5 +70,11 @@ ggplot(df_ccf_11_diff) +
   geom_col(aes(x = Lag, y = CCF), width = 0.2) +
   labs(title = "Daily cross-correlation - lagged differences") +
   theme_bw()
+
+# We can also examine the autocorrelation of the data
+acf(cc11$GPP)
+acf(diff_c11_gpp)
+# Like the adf.test function above, the acf function does not play
+# nice with datasets with missing data.
 
 # End of script.
