709a88ac716a6c276d493bd79c45fd43fb12f3c0
Alice Carter <alicecarter05@gmail.com>
2021-11-18 11:17:45 -0700

    added script for comparing filtered powell data to savoy data and to select annual autotrophic sites

---
+++ R/extract_powell_autotrophic_rivers.R
@@ -0,0 +1,87 @@
+# Explore autotrophy in the cleaned and gap filled dataset from P Savoy 
+
+# A Carter
+# 2021-09
+
+setwd('C:/Users/Alice Carter/git/except_heterotrophy/')
+library(tidyverse)
+library(lubridate)
+
+
+# Filter Powell Center data set ####
+dat <- read.table('data_ignored/daily_predictions.tsv', header = TRUE)
+
+# Remove negative GPP, positive ER, and Rhat > 1.05
+filtered <- dat %>%
+  mutate(date = as.Date(date),
+         year = year(date)) %>%
+  filter(GPP >= 0, ER <= 0, 
+         GPP.Rhat < 1.05, ER.Rhat < 1.05, K600.Rhat < 1.05)# %>%
+  # mutate(GPP = case_when(GPP < 0 ~ 0, TRUE ~ GPP),
+  #        ER = case_when(ER > 0 ~ 0, TRUE ~ ER)) 
+
+
+# Compare filtered data set to the data currently being used in the SP paper 
+#  (Bernhardt Savoy et al paper, data from mvlah 10/2021)
+# 
+sp_dat <- read_csv('../loticlentic_synthesis/data/phil_powell_data/lotic_gap_filled_unlisted.csv') %>%
+  rename(site_name = sitecode, date = Date, year = Year)
+
+c_years <- filtered %>%
+  group_by(site_name, year) %>%
+  filter(sum(!is.na(GPP))/365 >= 0.6,
+         sum(!is.na(ER))/365 >= 0.6) %>%
+  ungroup()
+
+sp_only <- anti_join(sp_dat, c_years, by = c('site_name', 'date')) %>%
+  filter(!is.na(GPP), !is.na(ER), grepl('nwis', site_name))
+pw_only <- anti_join(c_years, sp_dat, by = c('site_name', 'date')) %>%
+  filter(!is.na(GPP), !is.na(ER))
+
+summary(pw_only)
+# compare filtered and unfiltered data sets
+png('figures/filtered_data_comparison.png', height = 480, width = 640)
+  plot(dat$ER, dat$GPP, xlab = 'ER', ylab = 'GPP')
+  points(filtered$ER, filtered$GPP, col = 2, pch = 20)
+  points(pw_only$ER, pw_only$GPP, pch = 20, col = 4)
+  legend('topright', 
+         c('powell center', 'filtered data', 'data not included in Savoy data'),
+         pch = c(1, 20, 20), col = c(1,2,4), bty = 'n')
+dev.off()
+nrow(dat)
+nrow(filtered)
+nrow(pw_only)
+
+# it looks like the savoy dataset excludes an additional ~10% of the data, but
+# they don't look like an autotrophically interesting subset, so likely it 
+# wouldn't change our analysis.
+# However, I do think it will still be useful to retain the info from the 
+# gap filled savoy data set for calculating annual rates where possible. 
+# To that end, I've created a summary dataframe below and added it to the 
+# working data folder.
+# summarize annual values ####
+# 
+# annual <- sp_dat %>%
+#   group_by(site_name, year) %>%
+#   summarize(across(ends_with('filled'), sum)) %>%
+#   rename_with(function(x) sub('_filled', '', x), everything()) %>%
+#   select(-ends_with('_C'))
+# 
+# write_csv(annual, 'data_working/annual_gapfilled_values.csv')
+annual <- read_csv('data_working/annual_gapfilled_values.csv')
+
+# using only the years with at least 60% of data, which sites are autotrophic 
+# at the annual timescale? 
+
+auto_siteyears <- annual %>%
+  filter(NEP > 0) %>%
+  rename_with(.fn = function(x) paste0(x, '_annual'), 
+              .cols = c(-site_name, -year)) %>%
+  mutate(siteyear = paste(site_name, year, sep = '_'))
+
+auto_sites <- c_years %>% 
+  filter(paste(site_name, year, sep = '_') %in% auto_siteyears$siteyear) %>%
+  left_join(auto_siteyears, by = c('site_name', 'year')) %>%
+  select(-siteyear)
+
+write_csv(auto_sites, 'data_working/autotrophic_siteyears_daily.csv')
