968fb21a517a9e608f2d585c189f78524bf1c01f
Melissa <mdesierv@uwyo.edu>
2022-05-26 11:53:06 -0600

    summarized across siteyears

--- R/Fig1map.R
+++ R/Fig1map.R
@@ -8,4 +8,8 @@ riversiteyears<-read.csv("data_working/across_sites_model_data.csv")
 
 riversiteyears_2<-riversiteyears %>% dplyr::select(site_name, year, lat, lon, ann_GPP_C, ann_ER_C) %>% mutate(NEP=(abs(ann_GPP_C) - abs(ann_ER_C))) %>% mutate(PtoR = abs(ann_GPP_C) / abs(ann_ER_C))
 
-rivesite<-
+riversite<- riversiteyears_2 %>%  group_by(site_name, lat, lon)  %>% summarise(NEP=mean(NEP), PtoR=mean(PtoR), nyears = n())
+
+riversite<-write.csv("data_working/riversiteannual.csv")
+
+                                                                               
