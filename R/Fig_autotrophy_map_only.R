6e3fe4fd478fc876a77ff306ce96bdfa44ef694f
Carolina Barbosa <carolina.cbarbosa@usp.com>
2022-05-27 10:29:48 -0600

    New version

--- R/Fig_autotrophy_map_only.R
+++ R/Fig_autotrophy_map_only.R
@@ -8,14 +8,17 @@ lapply(c("plyr","dplyr","ggplot2","cowplot",
          "ggmap","maps","mapdata",
          "ggsn","wesanderson"), require, character.only=T)
 
+library(dplyr)
 
 ## Import Data
-NEP_info <- read.csv("")
+NEP_info <- read.csv("data_working/riversiteannual.csv")
+NEP_info_2<-NEP_info %>% mutate(logPtoP=log(PtoR))
+
 
 ## Generate map of NEP
 NEP_map_fig <- ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5, 
                              maptype='toner'))+
-    geom_point(data = NEP_info, aes(x = lon, y = lat, 
+    geom_point(data = NEP_info_2, aes(x = lon, y = lat, 
                                   fill=NEP, size=NEP), shape=21)+
     theme(legend.position = "right")+
     labs(x="Longitude", y="Latitude")+
@@ -26,17 +29,26 @@ NEP_map_fig <- ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5,
 ## Generate map of P:R
 PR_map_fig <- ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5, 
                     maptype='toner'))+
-  geom_point(data = NEP_info, aes(x = lon, y = lat, 
+  geom_point(data = NEP_info_2, aes(x = lon, y = lat, 
                                 fill=PtoR, size=PtoR), shape=21)+
   theme(legend.position = "right")+
   labs(x="Longitude", y="Latitude")+
   scale_fill_gradient("P:R",
-                      low = "blue", high = "red")+
+                      low = "green", high = "brown")+
   scale_size_continuous("P:R")
 
 
+PR_map_fig
+NEP_map_fig
+
+NEP_quantile <- quantile(NEP_info_2[,5], probs = seq(0, 1, 0.25), na.rm = FALSE,
+         names = TRUE)
 
 
+#library(ggExtra)
+#ggMarginal(PR_map_fig, data=NEP_info_2)
+
+NEP_hist <- hist(NEP_info_2[,8],main="logNEP",xlab="",xlim=c(-4,1),col="brown", ylim = c(0,100))
 
 
 
