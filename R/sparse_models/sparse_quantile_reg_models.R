94d8faaa46ae98c19771912ed99e9133a9c9f4a0
Alice Carter <alicecarter05@gmail.com>
2023-04-10 16:51:26 -0600

    made plots of quantile model regression fits and built data table of covariates for SI

--- R/sparse_models/sparse_quantile_reg_models.R
+++ R/sparse_models/sparse_quantile_reg_models.R
@@ -59,6 +59,30 @@ lqmod <- quantreg::rqss(fmlqmod,
                         data = dd)
 summary(lqmod, se = 'boot')
 qq <- summary(lqmod, se = 'boot')
+dd$NEPpred <- predict(lqmod, dd[,-5])
+
+library(viridis)
+png('figures/sparse_quantile_model_predictions.png',
+    width = 6, height = 4.5, units = 'in', res = 300)
+  dd %>%
+    select(Width, Elevation = ElevWs, `Terrestrial NPP` = MOD_ann_NPP, 
+           `Max interstorm interval` = max_interstorm) %>%
+    mutate(across(.fns = ~ (. - min(.))/(max(.)-min(.)))) %>% 
+    bind_cols(select(dd, NEP, NEPpred)) %>% 
+    pivot_longer(cols = any_of(c('Width', 'Elevation', 'Terrestrial NPP', 
+                          'Max interstorm interval')),
+                 values_to = 'Value', names_to = 'covariate') %>%
+  ggplot(aes(NEP, NEPpred, col = Value))+
+    geom_abline(slope = 1, intercept = 0, lty = 2, col = 'grey50')+
+    geom_point() +
+    scale_color_continuous(type = 'viridis')+
+    ylab(expression('Predicted NEP (g '~ O[2]~ m^2~ d^-1* ')'))+
+    xlab(expression('NEP (g '~ O[2]~ m^2~ d^-1* ')'))+
+    facet_wrap(.~covariate)+
+    theme_classic() +
+    theme(panel.border = element_rect(fill = NA))
+dev.off()
+
 colnames(qq$coef)<- c('value', 'se', 't_val', 'p_val')
 coefs <- data.frame(qq$coef) %>%
   arrange((abs(value))) %>%
@@ -87,7 +111,7 @@ dev.off()
 qrtab <- data.frame(qq$coef) %>%
   mutate(variable = row.names(qq$coef)) %>% tibble() 
 
-tmp <-dat %>%
+tmp <- dat %>%
   filter(!is.na(site_name))%>%
   group_by(site_name) %>%
   summarize(across(where(is.numeric), median, na.rm = T)) %>%
@@ -97,29 +121,41 @@ tmp <-dat %>%
          !is.na(PrecipWs),
          !is.na(Stream_PAR_sum),
          !is.na(width_to_area)) %>%
-  select(-ann_GPP_C, -ann_ER_C, -NHD_TIDAL, -site_name, -year, -lat, -lon, -NEP, -PR)  %>%
-  summary()
-
-  summarize_all(.funs = list(min, max)) %>%
+  select(-ann_GPP_C, -ann_ER_C, -NHD_TIDAL, -site_name, -year,
+         -lat, -lon, -NEP, -PR)  %>%
+  # summarize(across(everything(), min)) %>%
+  summarize(across(everything(), .fns = list(min, max))) %>%
   pivot_longer(cols = everything(), values_to = 'val', 
-               names_to = c('Variable', 'stat'), 
-               names_pattern = '(^[A-Za-z0-9*]+)_(fn[12]$)')
-  
-write_csv(qrtab, 'data_working/sparse_quantile_regression_results_raw.csv')
+               names_to = c('variable', 'stat'), 
+               names_pattern = '^(.+)?_([12])$') %>%
+  pivot_wider(id_cols = variable, names_from = stat, values_from = val) %>%
+  rename(min = `1`, max = `2`)
+
+qrtab <- qrtab %>%
+  select(variable) %>%
+  left_join(tmp, by = 'variable')
+tab_ord <- read_csv('data_working/sparse_quantile_regression_results.csv')
+qrtab <- left_join(tab_ord, qrtab)
+write_csv(qrtab, 'data_working/sparse_quantile_regression_results.csv')
 
 qrtab <- read_csv('data_working/sparse_quantile_regression_results.csv')
-install.packages('kableExtra')
+# install.packages('kableExtra')
 library(kableExtra)
+options(scipen = 0)
 
 table <- qrtab %>% select(-variable) %>%
   mutate(estimate = round(estimate, digits = 3),
          se = paste0(" $pm ",round(se, digits = 2)),
          t_val = round(t_val, digits = 2),
-         p_val = round(p_val, digits = 2)) 
+         p_val = round(p_val, digits = 2),
+         min = as.character(signif(min, digits = 2)),
+         max = paste0('- ', as.character(signif(max, digits = 2)))) %>%
+  relocate(Units, .after = max)
 
 table %>%
   kbl(caption = 'Covariates and coefficient estimates for sparse quantile regression model',
       format = 'latex',
-      col.names = c("Variable", "Estimate",  "$pm se", "t value", "p value"), 
-      align = c('l', 'r', 'l', 'r', 'r')) %>%
+      col.names = c("Variable", "Estimate",  "$pm se", "t value", "p value",
+                    "", "range", ""), 
+      align = c('l', 'r', 'l', 'r', 'r', 'r', 'l', 'l')) %>%
   kable_classic(full_width = F, html_font = 'helvetica')
