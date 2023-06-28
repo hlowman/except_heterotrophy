9efe931a99e2fa32befe54dac4eb1e71dcfb0307
Melissa <mdesierv@uwyo.edu>
2023-06-28 13:33:07 -0600

    added figure w/ literatre sites.

--- R/others_scripts/Dunkle_constrained_PCA_annualPR.R
+++ R/others_scripts/Dunkle_constrained_PCA_annualPR.R
@@ -1,13 +1,15 @@
 ## Matt Dunkle made this code May 29 2023 ##
 ## Melissa D. edited on June 27 2023 ##
 
-
-
 #data = across_sites_model_data#
 
-
-
-library(tidyverse);library(vegan);library(ggvegan);library(BiodiversityR);library(ggforce);library(ggrepel); library(here)
+library(tidyverse);
+library(vegan);
+#library(ggvegan);
+library(BiodiversityR);
+library(ggforce);
+library(ggrepel); 
+library(here)
 
 
 across_sites_model_data <- read.csv(here::here("data_working/across_sites_model_data.csv"), header=TRUE, fileEncoding = "UTF-8-BOM")
@@ -29,10 +31,12 @@ autotrophic = across_sites_model_data %>%
 
 autotrophic_data = autotrophic %>% select( Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width,Stream_PAR_sum)
 
+autotrophic_data2 = autotrophic %>% select( site_name, year, Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width,Stream_PAR_sum)
+
+
+litauto<-dplyr::bind_rows(autotrophic, literature_data)
 
 ## additional dataframes #
-
-autotrophic_data2 = autotrophic %>% select( site_name, year, Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width,Stream_PAR_sum)
 
 autotrophic_QR = autotrophic %>% select( site_name, year, Stream_PAR_sum, max_interstorm, Disch_cv, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected)
 
@@ -213,4 +217,66 @@ rda_QR <- ggplot() +
 
 ##
 
-watershed_summary_data %>% filter(site_name == "nwis_13173600") %>% view()      
+## WITH BOTH THE LITERATURE AND OUR AUTOROPHIC SITES
+
+head(litauto)
+
+litauto2<-litauto %>% select(site_name,year, ann_GPP_C , PR, Class, River, Type1, Type2, nwis_gage, Disch_cv:Stream_PAR_sum)
+
+litauto3<-litauto2[,10:20]
+
+pca_litauto = rda(litauto3,  scale=T)  ## 48 % var 2 axes ##
+
+plotlitautopca<-plot(pca_litauto)
+
+litautositeslong <- sites.long(plotlitautopca, env.data=litauto2[,1:9]) #extract data for plotting
+
+litautospecieslong <- species.long(plotlitautopca)
+
+##need to split up the sites dataframes into lit/auto again for plotting purposes###
+
+litsiteslong2<-litautositeslong %>% filter(nwis_gage>0) %>% select(River, Type1, Type2, nwis_gage, axis1, axis2, labels)
+
+autositeslong2<-litautositeslong %>% filter(year>0) %>% select(site_name, year, ann_GPP_C, PR, Class, axis1, axis2, labels)
+
+
+rda_litauto <- ggplot() + 
+  geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
+  geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +  
+  scale_x_continuous(sec.axis = dup_axis(labels=NULL, name=NULL)) +
+  scale_y_continuous(sec.axis = dup_axis(labels=NULL, name=NULL)) +    
+  
+  geom_point(data=litsiteslong2, aes(x=axis1, y=axis2), color="blue", 
+             size=2, show.legend = T) +
+  geom_text(data = litsiteslong2, 
+            mapping = aes(x=axis1, y=axis2, label = Type1), 
+            inherit.aes = FALSE, color="blue") +
+  geom_point(data=autositeslong2 %>%  mutate(Class = case_when(PR<1~"Heterotrophic",
+                                                              PR>1~"Autotrophic")), 
+             aes(x=axis1, y=axis2, colour=PR, shape = Class, alpha = Class), 
+             size=5, show.legend = T) +
+  coord_fixed(ratio=1)+
+  scale_alpha_manual(values = c(1,0.1))+
+  
+  geom_mark_ellipse(data=autositeslong2%>%  mutate(Class = case_when(PR<1~"Heterotrophic",
+                                                                     PR>1~"Autotrophic")),
+                    aes(x=axis1, y=axis2, linetype = Class),
+                    expand=0, size=0.2, show.legend=T, alpha =.1) +
+  geom_segment(data=litautospecieslong# %>% mutate(axis1 = case_when(labels == "Width"~-1,T~axis1),
+               # axis2 = case_when(labels == "ElevWs"~1,T~axis2),
+               # weight = case_when(labels %in% c("ElevWs","Width")~2,T~1))
+               ,
+               aes(xend=axis1, y = 0,x=0, yend=axis2),
+               colour="red",  arrow=arrow(), show.legend = F) +
+  
+  geom_text_repel(data=litautospecieslong #%>% mutate(axis1 = case_when(labels == "Width"~-1,T~axis1),
+                  #          axis2 = case_when(labels == "ElevWs"~1,T~axis2))
+                  ,
+                  aes(x=axis1, y=axis2, label=labels),
+                  colour="black", check.overlap = T, show.legend = F)+
+  scale_colour_stepsn(colors = terrain.colors(10, rev=T))+
+  theme_bw()+theme(panel.grid = element_blank())+ggtitle("PCA that includes literature sites")
+
+
+
+
