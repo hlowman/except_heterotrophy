10b2e5367c7558220f85eed20792a1623229a42c
Matthew Dunkle <104026488+matthewrdunkle@users.noreply.github.com>
2024-03-30 13:11:19 -0600

    Update Dunkle_constrained_PCA_annualPR.R
    Updated with what I have on my end!

--- R/others_scripts/Dunkle_constrained_PCA_annualPR.R
+++ R/others_scripts/Dunkle_constrained_PCA_annualPR.R
@@ -13,11 +13,9 @@ library(ggrepel);
 library(here)
 
 
-across_sites_model_data <- read.csv(here::here("data_working/across_sites_model_data.csv"), 
-                                    header=TRUE, fileEncoding = "UTF-8-BOM")
+across_sites_model_data <- read.csv(here::here("data_working/across_sites_model_data.csv"), header=TRUE, fileEncoding = "UTF-8-BOM")
 
-literature_data <- read.csv(here::here("data_working/literature_streams_data_for_PCA.csv"), 
-                            header=TRUE, fileEncoding = "UTF-8-BOM")
+literature_data <- read.csv(here::here("data_working/literature_streams_data_for_PCA.csv"), header=TRUE, fileEncoding = "UTF-8-BOM")
 
 literature_data2 <- literature_data %>%  mutate(Class = "Literature")
 
@@ -30,9 +28,9 @@ autotrophic = across_sites_model_data %>%
   summarize(max_NEP = max(ann_NEP_C, na.rm = T),
             across(where(is.numeric), \(x) median(x, na.rm = T))) %>% 
   ungroup() %>% 
-  select(site_name, lat, lon, ann_GPP_C, ann_ER_C, ann_NEP_C, max_NEP, PR, 
-         Stream_PAR_sum, max_interstorm, MOD_ann_NPP, PAR_kurt, ElevWs, Width) %>% 
-  # select(site_name, year, lat, lon, ann_GPP_C, ann_ER_C, ann_NEP_C,  PR,PAR_sum, Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width, Stream_PAR_sum) %>% 
+  # select(site_name, lat, lon, ann_GPP_C, ann_ER_C, ann_NEP_C, max_NEP, PR, 
+  #        Stream_PAR_sum, max_interstorm, MOD_ann_NPP, PAR_kurt, ElevWs, Width) %>% 
+  select(site_name, year, lat, lon, ann_GPP_C, ann_ER_C, ann_NEP_C,  PR,PAR_sum, Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width, Stream_PAR_sum, max_NEP) %>%
   filter(complete.cases(across(ann_GPP_C:Width))) %>% as_tibble() %>% 
   mutate(Class = case_when(PR > 1 ~ "Autotrophic", 
                            max_NEP > 0 ~ "Sometimes Autotrophic",
@@ -41,26 +39,18 @@ autotrophic = across_sites_model_data %>%
 
 
 
-autotrophic_data = autotrophic %>% 
-  select( Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, 
-          drainage_density_connected, PAR_kurt, ElevWs, Width,Stream_PAR_sum)
+autotrophic_data = autotrophic %>% select( Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width,Stream_PAR_sum)
 
-autotrophic_data2 = autotrophic %>% 
-  select( site_name, year, Disch_cv, max_interstorm, RBI, width_to_area, 
-          PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs,
-          Width,Stream_PAR_sum)
+autotrophic_data2 = autotrophic %>% select( site_name, year, Disch_cv, max_interstorm, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected, PAR_kurt, ElevWs, Width,Stream_PAR_sum)
 
 
 litauto<-dplyr::bind_rows(autotrophic, literature_data2)
 
 ## additional dataframes #
 
-autotrophic_QR = autotrophic %>% 
-  select( site_name, Stream_PAR_sum, max_interstorm, Disch_cv, RBI,
-          width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected)
+autotrophic_QR = autotrophic %>% select( site_name, year, Stream_PAR_sum, max_interstorm, Disch_cv, RBI, width_to_area, PrecipWs, MOD_ann_NPP, drainage_density_connected)
 
-autotrophic_sparse = autotrophic %>% 
-  select( site_name, year, max_interstorm, MOD_ann_NPP, PAR_kurt, ElevWs, Width)
+autotrophic_sparse = autotrophic %>% select( site_name, year, max_interstorm, MOD_ann_NPP, PAR_kurt, ElevWs, Width)
 
 
 #auto_hellinger = disttransform(autotrophic_data, method = "log")
@@ -241,17 +231,24 @@ rda_QR <- ggplot() +
 
 head(litauto)
 
-litauto2<-litauto %>% select(site_name,year, ann_GPP_C , PR, Class, River, Type1, Type2, nwis_gage, Disch_cv:Stream_PAR_sum)
+litauto2<-litauto %>% mutate(primary_class = case_when(is.na(primary_class) == T~Class, T~primary_class)) %>% 
+                      mutate(secondary_class = case_when(is.na(secondary_class)==T~ "none", T~secondary_class)) %>%
+                      mutate(Group = case_when(is.na(Group) == T~Class, T~Group)) %>% 
+                      mutate(dam = case_when(dam == 0~"no_dam", 
+                                             dam ==1 ~"dam", 
+                                             is.na(dam)==T~ "unknown")) 
+                
+                  # select(site_name,year, ann_GPP_C , PR, Class, River, Type1, Type2, nwis_gage, Disch_cv:Stream_PAR_sum)
 
 litauto3<-litauto2[,10:20]
 
 pca_litauto = rda(litauto3,  scale=T)  ## 48 % var 2 axes ##
 
-summary(pca_litauto) %>% selec
+summary(pca_litauto) 
 
 plotlitautopca<-plot(pca_litauto)
 
-litautositeslong <- sites.long(plotlitautopca, env.data=litauto2[,1:9]) #extract data for plotting
+litautositeslong <- sites.long(plotlitautopca, env.data=litauto2)#[,1:9]) #extract data for plotting
 
 litautospecieslong <- species.long(plotlitautopca)
 
@@ -259,27 +256,36 @@ litautospecieslong <- species.long(plotlitautopca)
 
 litsiteslong2<-litautositeslong %>% filter(nwis_gage>0) %>% select(River, Type1, Type2, nwis_gage, axis1, axis2, labels)
 
-autositeslong2<-litautositeslong %>% filter(year>0) %>% select(site_name, year, ann_GPP_C, PR, Class, axis1, axis2, labels)
+autositeslong2<-litautositeslong %>% filter(year>0) %>% 
+                        mutate(Type1 = case_when(primary_class == "Heterotrophic"~"Heterotrophic",
+                                                 primary_class == "urban"~"Urban",
+                                                 primary_class == "agri" ~"Agricultural",
+                                                 primary_class == "desert" ~ "Desert",
+                                                 primary_class == "snowmelt" ~ "Snowmelt"))
+                          #%>% select(site_name, year, ann_GPP_C, PR, Class, axis1, axis2, labels)
 
 
-rda_litauto <-  ggplot() + 
+rda_litauto <-  ggplot(data=autositeslong2 %>%  mutate(Class = case_when(PR<1~"Heterotrophic",
+                                                                         PR>1~"Autotrophic")) %>%
+                         rename(`Annual Trophic State` = Class),
+                       aes(x=axis1, y=axis2)) + 
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
   geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +  
   scale_x_continuous(sec.axis = dup_axis(labels=NULL, name=NULL)) +
   scale_y_continuous(sec.axis = dup_axis(labels=NULL, name=NULL)) +    
+  geom_mark_ellipse(aes(fill = `Annual Trophic State`, label = `Annual Trophic State`, linetype = `Annual Trophic State`, alpha  = `Annual Trophic State` ),
+                    expand=0, alpha =.1, show.legend = FALSE) +
+  scale_fill_manual(values = c("green4","tan2"))+
   
   
-  geom_point(data=autositeslong2 %>%  mutate(Class = case_when(PR<1~"Heterotrophic",
-                                                              PR>1~"Autotrophic")), 
-             aes(x=axis1, y=axis2, colour=PR, shape = Class, alpha = Class), 
-             size=5, show.legend = T) +
-  coord_fixed(ratio=1)+
-  scale_alpha_manual(values = c(1,0.1))+
+  geom_point(data= autositeslong2 %>% mutate(Class = case_when(PR<1~"Heterotrophic", PR>1~"Autotrophic")) %>% 
+               rename(Classification = Type1, `Annual P:R` = PR), 
+             aes(x=axis1, y=axis2, colour=`Annual P:R`, shape = Classification), 
+             size=5, show.legend = NA) +
+      scale_alpha_manual(values = c(1,0.75))+
+
+
   
-  geom_mark_ellipse(data=autositeslong2%>%  mutate(Class = case_when(PR<1~"Heterotrophic",
-                                                                     PR>1~"Autotrophic")),
-                    aes(x=axis1, y=axis2, linetype = Class),
-                    expand=0, size=0.2, show.legend=T, alpha =.1) +
   geom_segment(data=litautospecieslong# %>% mutate(axis1 = case_when(labels == "Width"~-1,T~axis1),
                # axis2 = case_when(labels == "ElevWs"~1,T~axis2),
                # weight = case_when(labels %in% c("ElevWs","Width")~2,T~1))
@@ -333,15 +339,19 @@ rda_litauto <-  ggplot() +
                                                                         0)),
                   colour="red3", check.overlap = T, show.legend = F)+
   scale_colour_stepsn(colors = terrain.colors(10, rev=T))+
-    geom_point(data=litsiteslong2, aes(x=axis1, y=axis2), color="blue", 
-               size=2, show.legend = F, shape = 19) +
-    geom_text(data = litsiteslong2, 
+    geom_point(data=litsiteslong2, aes(x=axis1, y=axis2, shape = Type1, color = "Literature"), color="blue", fill = "blue",
+               size=5, show.legend = F, alpha = .5) +
+    geom_text_repel(data = litsiteslong2, 
               mapping = aes(x=axis1, y=axis2, label = River), 
-              inherit.aes = FALSE, color="blue", position = position_nudge(y = .085, set.seed(5))) +
-  theme_bw()+theme(panel.grid = element_blank())+
-    labs(x = "pca1 (32.34%)", y = "pca2 (16.88%)")+coord_cartesian()
+              inherit.aes = FALSE, color="blue",force = 2, force_pull = 10, max.time = 4) +
+        theme_bw()+theme(panel.grid = element_blank(), legend.direction = "vertical",legend.text.align =0, 
+                   legend.position = c(.8,.15), legend.box.just = "right", legend.box = "horizontal")+
+    labs(x = "pca1 (32.34%)", y = "pca2 (16.88%)")+coord_equal(ratio = 1, xlim = c(-2,2))+
+  scale_shape_manual(values = c(18,25, 17, 1,8, 10, 15))
 
-save_plot("Heterotrophy_Figure6_PCA.png", rda_litauto, dpi = 600, base_width = 8, base_height = 6)
+rda_litauto
+
+cowplot::save_plot("Heterotrophy_Figure6_PCA.png", rda_litauto, dpi = 600, base_width = 8, base_height = 8)
 
 ggplot(litauto, aes(x = Disch_cv, fill = Class)) + 
   geom_histogram()
